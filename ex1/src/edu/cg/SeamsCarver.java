9b25d31d56c81f52b41fbbf3e72b1685b5cae07f
danpollak <dandan888@gmail.com>
2018-04-14 10:43:56 +0300

    added seam collector

--- ex1/src/edu/cg/SeamsCarver.java
+++ ex1/src/edu/cg/SeamsCarver.java
@@ -2,7 +2,10 @@ package edu.cg;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class SeamsCarver extends ImageProcessor {
 	
@@ -17,7 +20,7 @@ public class SeamsCarver extends ImageProcessor {
 	private ResizeOperation resizeOp;
 	private long[][] m;
 	private long[][] e;
-	private Collection seams;
+	private int[][] seams;
 	
 	//TODO: Add some additional fields:
 	
@@ -43,9 +46,7 @@ public class SeamsCarver extends ImageProcessor {
 			resizeOp = this::reduceImageWidth;
 		else
 			resizeOp = this::duplicateWorkingImage;
-		
-		// TODO: Format seams collection
-		
+				
 		// Initialize both previous and current matrices as gradient magnitude.
 		m = new long[gradientMag.getWidth()][gradientMag.getHeight()];
 		e = new long[gradientMag.getWidth()][gradientMag.getHeight()];
@@ -53,16 +54,62 @@ public class SeamsCarver extends ImageProcessor {
 			m[x][y] = new Color(gradientMag.getRGB(x, y)).getRed();
 			e[x][y] = new Color(gradientMag.getRGB(x, y)).getRed();
 		});
-		logger.log("done intializing");
 		// Run matrix
-		logger.log("dim y:" + m.length + " dim x:" + m[0].length);
 		for(int y=1;y<m.length;y++) {
 			for(int x=0;x<m[0].length;x++) {
 				m[y][x]+= calcEnergy(y,x);
 			}
 		}
-		logger.log("completed");
 		
+		// extract k seams
+		seams = new int[numOfSeams][m.length];
+		int[][] indexTracking = new int[m.length][m[0].length];
+		
+		for(int k=0;k<numOfSeams;k++) {
+			long[] topRow = m[m.length-1];
+			int[] minimalSeam = new int[m.length];
+			int minimalIndex = 0;
+			// find the minimal index in the top row
+			for(int i=1;i<topRow.length;i++) {
+				if(topRow[minimalIndex] < topRow[i]) {
+					minimalIndex = i;
+				}
+			}	
+			// backtrack minimal energy seam
+			minimalSeam[m.length-1] = minimalIndex + indexTracking[m.length-1][minimalIndex];
+			logger.log("backtrack");
+			for(int j=m.length-1;j>0;j--) {
+				// choose the minimal value for each previous 3 index
+				long leftIndex = minimalIndex > 0 ? m[j-1][minimalIndex-1] : Long.MAX_VALUE;
+				long midIndex = m[j-1][minimalIndex];
+				long rightIndex = minimalIndex >= topRow.length ? m[j-1][minimalIndex+1] : Long.MAX_VALUE;
+				if(leftIndex < midIndex && leftIndex < rightIndex) {
+					minimalIndex--;
+				} else if (rightIndex < midIndex && rightIndex < leftIndex) {
+					minimalIndex++;
+				}
+				minimalSeam[j-1] = minimalIndex + indexTracking[j-1][minimalIndex];
+			}
+			// put the seam in the seam collection
+			seams[k] = minimalSeam;
+			// remove seam, update index and calculate new energy
+			logger.log("remove seam");
+			for(int j=0;j<m.length;j++) {
+				long[] newRow = new long[m[0].length-1];
+				for(int i=0;i<m[0].length-1;i++) {
+					if(i<minimalSeam[j]) {
+						// if you haven't hit the removed index, copy
+						newRow[i] = m[j][i];
+					} else {
+						// if you are above the removed index, skip one and indicate it on the indexTracking
+						newRow[i] = m[j][i+1];
+						indexTracking[j][i]++;
+					}
+				}
+				// point the matrix to the new row
+				m[j] = newRow;
+			}	
+		}
 	}
 	
 	public long calcEnergy(int y, int x) {
