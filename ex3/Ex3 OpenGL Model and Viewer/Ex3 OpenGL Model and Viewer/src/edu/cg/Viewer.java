7bf0a41c2e5985c206d59b9196a36ed4a2f8696b
Gal Shnapp <gal.shnapp@gmail.com>
2018-06-03 13:04:11 +0300

    Viewer imp

--- ex3/Ex3 OpenGL Model and Viewer/Ex3 OpenGL Model and Viewer/src/edu/cg/Viewer.java
+++ ex3/Ex3 OpenGL Model and Viewer/Ex3 OpenGL Model and Viewer/src/edu/cg/Viewer.java
@@ -12,104 +12,126 @@ import java.awt.Point;
 
 public class Viewer
 		implements GLEventListener {
-	private double zoom;
-	private Point mouseFrom;
-	private Point mouseTo;
-	private int canvasWidth;
-	private int canvasHeight;
-	private boolean isWireframe = false;
-	private boolean isAxes = true;
-	private IRenderable model;
-	private FPSAnimator ani;
-	private double[] rotationMatrix = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0};
-	private Component glPanel;
-	private boolean isModelCamera = false;
-	private boolean isModelInitialized = false;
+	private double zoom; //How much to zoom in? >0 mean magnify, <0 means shrink
+	private Point mouseFrom,mouseTo; //From where to where was the mouse dragged between the last redraws?
+	private boolean isWireframe = false;//Should we display wireframe or not?
+	private boolean isAxes = true; //Should we display axes or not?
+	private IRenderable model;//Model to display
+	private FPSAnimator ani; //This object is responsible to redraw the model with a constant FPS
+	private Component glPanel; //We store the OpenGL panel component object to refresh the scene
+	private boolean isModelCamera = false;//Whether the camera is relative to the model, rather than the world (ex6)
+	private boolean isModelInitialized = false;//Whether model.init() was called.
+	private int width, height;//the frustum's borders!
+	private double[] rotationMatrix = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0};   //always start with I4X4
 
 	public Viewer(Component glPanel) {
 		this.glPanel = glPanel;
 		this.zoom = 0.0;
 	}
 
-	@Override
+	
 	public void display(GLAutoDrawable drawable) {
 		GL2 gl = drawable.getGL().getGL2();
 		if (!this.isModelInitialized) {
 			this.initModel(gl);
 			this.isModelInitialized = true;
 		}
-		gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-		gl.glClear(16640);
+		// gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f); //BackGround could be black or white!
+		gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
+		//gl.glClear(16640);
 		gl.glMatrixMode(5888);
 		this.setupCamera(gl);
 		if (this.isAxes) {
 			this.renderAxes(gl);
 		}
-		if (this.isWireframe) {
-			gl.glPolygonMode(1032, 6913);
+		
+		if (this.isWireframe) {	
+			gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE);
 		} else {
-			gl.glPolygonMode(1032, 6914);
+			gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL);
 		}
 		this.model.render(gl);
-		gl.glPolygonMode(1032, 6914);
+		gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL);
 	}
 
-	private Vec mousePointToVec(Point pt) {
-		double x = (double)(2 * pt.x) / (double)this.canvasWidth - 1.0;
-		double y = 1.0 - (double)(2 * pt.y) / (double)this.canvasHeight;
-		double z2 = 2.0 - x * x - y * y;
-		if (z2 < 0.0) {
-			z2 = 0.0;
+	private Vec projectToSphere(Point p){
+		System.out.println("projecting points to plane");
+		double planerX = (double)(2 * p.x) / (double) width - 1.0;
+		double planerY = 1.0 - (double)(2 * p.y) / (double) height;
+		double z = 2 - (planerX* planerX) - (planerY* planerY);
+		z = Math.max(0,z);
+		z = Math.sqrt(z);
+		return new Vec(planerX, planerY, z).normalize();
+	}
+	
+	private void trackBallRotateModel(GL2 gl, Point initial, Point finalp){
+		System.out.println("Rotating the model for trackBall");
+		Vec initailProjection;
+		Vec finalProjection;
+		Vec normal;
+		
+		initailProjection = projectToSphere(initial);
+		finalProjection = projectToSphere(finalp);
+		normal = initailProjection.cross(finalProjection).normalize();
+		if(normal.isFinite()){
+			double angle = 57.29577951308232 * Math.acos(initailProjection.dot(finalProjection));
+			angle = Double.isFinite(angle) ? angle : 0.0;
+			gl.glRotated(angle, normal.x, normal.y, normal.z);
 		}
-		double z = Math.sqrt(z2);
-		return new Vec(x, y, z).normalize();
 	}
 
+	private void trackBallZoom(GL2 gl){
+		gl.glMultMatrixd(this.rotationMatrix, 0);
+		gl.glGetDoublev(2982, this.rotationMatrix, 0);
+		gl.glLoadIdentity();
+		gl.glTranslated(0.0, 0.0, -1.2);
+		gl.glTranslated(0.0, 0.0, - zoom);
+		gl.glMultMatrixd(this.rotationMatrix, 0);
+	}
+	
 	private void setupCamera(GL2 gl) {
+		// This implements the trackball!
+		gl.glLoadIdentity();
 		if (!this.isModelCamera) {
-			Vec to;
-			Vec from;
-			Vec axis;
-			gl.glLoadIdentity();
-			if (this.mouseFrom != null && this.mouseTo != null && (axis = (from = this.mousePointToVec(this.mouseFrom)).cross(to = this.mousePointToVec(this.mouseTo)).normalize()).isFinite()) {
-				double angle = 57.29577951308232 * Math.acos(from.dot(to));
-				angle = Double.isFinite(angle) ? angle : 0.0;
-				gl.glRotated(angle, axis.x, axis.y, axis.z);
-			}
-			gl.glMultMatrixd(this.rotationMatrix, 0);
-			gl.glGetDoublev(2982, this.rotationMatrix, 0);
-			gl.glLoadIdentity();
-			gl.glTranslated(0.0, 0.0, -1.2);
-			gl.glTranslated(0.0, 0.0, - this.zoom);
-			gl.glMultMatrixd(this.rotationMatrix, 0);
-			this.mouseFrom = null;
-			this.mouseTo = null;
+			if (this.mouseFrom != null && this.mouseTo != null) 
+					trackBallRotateModel(gl, mouseFrom,mouseTo);
+			trackBallZoom(gl);
+			
+			//By this point, we should have already changed the point of view, now set these to null
+			//so we don't change it again on the next redraw.
+			mouseFrom = null;
+			mouseTo = null;
 		} else {
-			gl.glLoadIdentity();
-			this.model.setCamera(gl);
+			model.setCamera(gl);
 		}
 	}
 
-	@Override
+	
 	public void dispose(GLAutoDrawable drawable) {
+		// TODO Typically there's nothing to do here
+		// well clearly, removing this method will make the world collapse.
 	}
 
-	@Override
+	
 	public void init(GLAutoDrawable drawable) {
 		GL2 gl = drawable.getGL().getGL2();
-		this.ani = new FPSAnimator(30, true);
-		this.ani.add(drawable);
-		this.glPanel.repaint();
-		this.initModel(gl);
+		
+		// Initialize display callback timer
+		//ani = new FPSAnimator(30, true);
+		//ani.add(drawable);
+		glPanel.repaint();
+		initModel(gl);
 	}
 
 	public void initModel(GL2 gl) {
-		gl.glCullFace(1029);
-		gl.glEnable(2884);
-		gl.glEnable(2977);
-		gl.glEnable(2929);
-		gl.glLightModelf(2898, 1.0f);
-		gl.glEnable(2896);
+		System.out.println("set culling, lighting, depthTest etc'...");
+		gl.glCullFace(GL2.GL_BACK);// Set Culling Face To Back Face
+		gl.glEnable(GL2.GL_CULL_FACE);// Enable back face culling
+		gl.glEnable(GL2.GL_DEPTH_TEST);// Enable Depth Test
+//		gl.glEnable(2977);
+//		gl.glEnable(2929);
+//		gl.glLightModelf(2898, 1.0f);
+//		gl.glEnable(2896);
 		this.model.init(gl);
 		if (this.model.isAnimated()) {
 			this.startAnimation();
@@ -118,21 +140,30 @@ public class Viewer
 		}
 	}
 
-	@Override
+	
 	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
 		GL2 gl = drawable.getGL().getGL2();
-		this.canvasWidth = width;
-		this.canvasHeight = height;
-		gl.glMatrixMode(5889);
+		// Remember the width and height of the canvas for the trackball.
+		this.width = width;
+		this.height = height;
+		
+		//Set the projection to perspective.
+		gl.glMatrixMode(GL2.GL_PROJECTION);
 		gl.glLoadIdentity();
-		gl.glFrustum(-0.1, 0.1, -0.1 * (double)height / (double)width, 0.1 * (double)height / (double)width, 0.1, 1000.0);
+		gl.glFrustum(-0.1,
+				0.1,
+				-0.1 * (double)height / (double)width,
+				0.1 * (double)height / (double)width,
+				0.1,
+				1000.0);
 	}
 
-	public void trackball(Point from, Point to) {
+	public void storeTrackball(Point from, Point to) {
 		if (!this.isModelCamera) {
 			if (this.mouseFrom == null) {
 				this.mouseFrom = from;
 			}
+			
 			this.mouseTo = to;
 			this.glPanel.repaint();
 		}
@@ -166,15 +197,15 @@ public class Viewer
 	}
 
 	public void startAnimation() {
-		if (!this.ani.isAnimating()) {
-			this.ani.start();
-		}
+		//if (!this.ani.isAnimating()) {
+		//		this.ani.start();
+		//	}
 	}
 
 	public void stopAnimation() {
-		if (this.ani.isAnimating()) {
-			this.ani.stop();
-		}
+		//	if (this.ani.isAnimating()) {
+		//		this.ani.stop();
+		//	}
 	}
 
 	private void renderAxes(GL2 gl) {
